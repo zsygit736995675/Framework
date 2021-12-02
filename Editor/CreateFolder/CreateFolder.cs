b7f3e5619fda58cfeb9f70475dc87cc2e3644670
guangchengzi <guangchengzi@newborntown.com>
2021-12-02 19:46:13 +0800

    目录整理

--- Editor/CreateFolder/CreateFolder.cs
+++ Editor/CreateFolder/CreateFolder.cs
@@ -2,29 +2,59 @@
 using UnityEditor;
 using System.IO;
 
-
-/// <summary>
-/// 创建文件夹  
-/// </summary>
-public class CreateFile
+namespace ZFrame
 {
-    [MenuItem("ZTools/创建默认文件夹")]//需要引入命名空间using UnityEditor
-    public static void CreateFolder()
+    /// <summary>
+    /// 创建文件夹  
+    /// </summary>
+    public class CreateFile
     {
-        string path = Application.dataPath + "/";
-        Directory.CreateDirectory(path + "Resources");//需要引入命名空间 using System.Io
-        Directory.CreateDirectory(path + "Resources/Texture");
+        [MenuItem("ZTools/创建默认文件夹")]//需要引入命名空间using UnityEditor
+        public static void CreateFolder()
+        {
+            string path = Application.dataPath + "/";
+            Directory.CreateDirectory(path + "Resources");
+            Directory.CreateDirectory(path + "Resources/Texture");
+            Directory.CreateDirectory(path + "Resources/Animation");
+            Directory.CreateDirectory(path + "Resources/Effect");
+            Directory.CreateDirectory(path + "Resources/Font");
+            Directory.CreateDirectory(path + "Resources/Audio");
+            Directory.CreateDirectory(path + "Resources/Audio/BGM");
+            Directory.CreateDirectory(path + "Resources/Audio/Sound");
+            Directory.CreateDirectory(path + "Resources/Prefabs");
+            Directory.CreateDirectory(path + "Resources/Models");
+            Directory.CreateDirectory(path + "Resources/Materials");
 
-        Directory.CreateDirectory(path + "Res");
-        Directory.CreateDirectory(path + "Res/Texture");
+            Directory.CreateDirectory(path + "Res");
+            Directory.CreateDirectory(path + "Res/Texture");
+            Directory.CreateDirectory(path + "Res/Animation");
+            Directory.CreateDirectory(path + "Res/Effect");
+            Directory.CreateDirectory(path + "Res/Font");
+            Directory.CreateDirectory(path + "Res/Audio");
+            Directory.CreateDirectory(path + "Res/Audio/BGM");
+            Directory.CreateDirectory(path + "Res/Audio/Sound");
+            Directory.CreateDirectory(path + "Res/Prefabs");
+            Directory.CreateDirectory(path + "Res/Models");
+            Directory.CreateDirectory(path + "Res/Materials");
 
-        Directory.CreateDirectory(path + "Plugins");
-        Directory.CreateDirectory(path + "Plugins/Android");
-        Directory.CreateDirectory(path + "Plugins/IOS");
+            Directory.CreateDirectory(path + "Plugins");
+            Directory.CreateDirectory(path + "Plugins/Android");
+            Directory.CreateDirectory(path + "Plugins/IOS");
 
-        Directory.CreateDirectory(path + "StreamingAssets");
-        Directory.CreateDirectory(path + "Editor");
-        Directory.CreateDirectory(path + "Scenes");
-        Directory.CreateDirectory(path + "Scripts");
+            Directory.CreateDirectory(path + "StreamingAssets");
+            Directory.CreateDirectory(path + "Editor");
+            Directory.CreateDirectory(path + "Scenes");
+            
+            Directory.CreateDirectory(path + "Scripts");
+            Directory.CreateDirectory(path + "Scripts/Main");
+            Directory.CreateDirectory(path + "Scripts/GamePlay");
+            
+            Directory.CreateDirectory(path + "3rdParty");
+            
+            
+            AssetDatabase.Refresh();
+        }
     }
 }
+
+
