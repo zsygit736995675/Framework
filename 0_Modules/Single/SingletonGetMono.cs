cfc2925a32509e3e89ed2089a927a9b4a117da79
git736995675zsy <736995675@qq.com>
2023-03-29 15:55:33 +0800

    单例修改

--- 0_Modules/Single/SingletonGetMono.cs
+++ 0_Modules/Single/SingletonGetMono.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 
-
+/// <summary>
+/// 一般为提前放置好的预制体使用
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class SingletonGetMono<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance = null;
@@ -8,14 +11,35 @@ public class SingletonGetMono<T> : MonoBehaviour where T : MonoBehaviour
     {
         get
         {
+            if( null == instance )
+                instance = Transform.FindObjectOfType<T>();
+
+            if( null == instance )
+            {
+                GameObject obj = new GameObject(typeof(T).Name);
+                instance = obj.AddComponent<T>();
+            }
+            
             return instance;
         }
     }
+    
     private void Awake()
     {
         if (instance == null)
         {
             instance = GetComponent<T>();
         }
+        
+        OnAwake();
     }
+    
+    private void Start()
+    {
+        OnStart();
+    }
+
+    public virtual void OnAwake() { }
+
+    public virtual void OnStart() { }
 }
